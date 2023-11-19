e25977277bdd0bb552ae5b8a71c81edad94db1bf
Ishtiaq Ahmed <awais9027prince@gmail.com>
2023-11-19 07:16:35 +0500

    update

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='flex-between background-light900_dark200 z-5 fixed w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12'>Navbar</nav>
   )
 }
 
